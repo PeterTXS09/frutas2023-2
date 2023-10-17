3319aa342b137b9f1263cf136eff93eb56ec5c1e
Peter Montalvo Garcia <43070511+PeterTXS09@users.noreply.github.com>
2023-10-17 06:09:09 +0000

    Update tf - final PrediccionWeb.py

--- PrediccionWeb.py
+++ PrediccionWeb.py
@@ -1,6 +1,6 @@
 import os
 import tempfile
-from flask import Flask, request, redirect, render_template, url_for
+from flask import Flask, request, redirect, render_template, url_for, flash
 from skimage import io
 import base64
 from skimage.transform import resize
@@ -18,7 +18,6 @@ def main():
 @app.route('/predict', methods=['POST'])
 def predict():
     try:
-        print("1!")
         img_data = request.form.get('myImage').replace("data:image/png;base64,","")
         img_binary = base64.b64decode(img_data)
         image = cv2.imdecode(np.frombuffer(img_binary, np.uint8), cv2.IMREAD_COLOR)
@@ -27,31 +26,23 @@ def predict():
         img_array = img_resized.reshape(1, 28, 28, 1)
         
         salida = model.predict(img_array)[0]
-        print(salida)
+
+        etiquetas = {0: "Manzana", 1: "Pera", 2: "Platano"}
+
+        valor = np.argmax(salida)
+
+        if valor in etiquetas:
+            kind = etiquetas[valor]
+            print(f"Kind: {kind}")
+            flash(f"Prediccion: {kind}")
+        else:
+            print("El valor predicho no tiene una etiqueta asociada.")
+            flash("El valor predicho no tiene una etiqueta asociada.")
         
-        nums = salida*100
-        print(nums)
-        numeros_formateados = [f'{numero:.2f}' for numero in nums]
-        print("13!")
-        cadena_formateada = ', '.join(numeros_formateados)
-        print("14!")
-        return redirect(url_for('show_predictions', nums=cadena_formateada, img_data=img_data))
     except Exception as e:
         print("Error occurred: ", e)
 
-    return redirect("/", code=302)
-
-@app.route('/predicciones')
-def show_predictions():
-    nums = request.args.get('nums')
-    img_data = request.args.get('img_data')
-    componentes = nums.split(', ')
-    nums = [float(componente) for componente in componentes]
-    frutas = ["Platano", "Pera", "Manzana"]
-    if img_data is not None:
-        return render_template('Prediccion.html', nums=nums, frutas=frutas, img_data=img_data)
-    else:
-        return redirect("/", code=302)
+    return redirect("/")
 
 
 if __name__ == "__main__":
